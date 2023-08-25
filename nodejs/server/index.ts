9b2a760eceea2d6fa2f8106b90b275b51dd3be2e
Gual Casas <gual.casas@gmail.com>
2023-08-24 21:18:03 -0600

    kill running server if attempting to start one

--- nodejs/server/index.ts
+++ nodejs/server/index.ts
@@ -3,10 +3,17 @@ import { startServer } from "./start-server.js";
 
 const socket = process.argv[2];
 
+async function killExisting(PORT: number) {
+    try {
+        await fetch(`http://localhost:${PORT}`, { method: "POST" });
+    } catch (e) {}
+}
+
 export async function main() {
     if (!socket) throw Error("missing socket");
     const nvim = attach({ socket });
     const PORT = Number(await nvim.getVar("markdown_preview_port"));
+    await killExisting(PORT);
     await startServer(nvim, PORT);
 }
 
