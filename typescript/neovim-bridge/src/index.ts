8205b1cabc4b483d0a30812834c2fbda064e2747
Gual Casas <gual.casas@gmail.com>
2023-09-10 17:15:54 -0600

    setup shared package

--- typescript/neovim-bridge/src/index.ts
+++ typescript/neovim-bridge/src/index.ts
@@ -1,9 +1,13 @@
+import { SHARED_VAL } from "@gc/shared/consts.ts";
 import { attach } from "neovim";
 
 const SOCKET = process.env["NVIM"];
+const VITE_GP_IS_DEV = process.env["VITE_GP_IS_DEV"];
 if (!SOCKET) throw Error("socket NVIM missing");
 
 const nvim = attach({ socket: SOCKET });
 
 const init = await nvim.getVar("github_preview_init");
-console.log("developmnet", init);
+console.log("init", init);
+console.log("VITE_GP_IS_DEV", VITE_GP_IS_DEV);
+console.log("SHARED_VAL", SHARED_VAL);
