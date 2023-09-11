ad7f5da9ba39118db69f8439f62876bae40e17a4
Gual Casas <gual.casas@gmail.com>
2023-09-10 18:41:10 -0600

    neovim bridge

--- typescript/neovim-bridge/src/index.ts
+++ typescript/neovim-bridge/src/index.ts
@@ -1,13 +1,48 @@
-import { SHARED_VAL } from "@gc/shared";
+import {
+    ENV,
+    EVENT_NAMES,
+    GP_UNIX_SOCKET_PATH,
+    type PluginInit,
+    type SocketEvent,
+} from "@gp/shared";
 import { attach } from "neovim";
+import { normalize } from "node:path";
 
 const SOCKET = process.env["NVIM"];
-const VITE_GP_IS_DEV = process.env["VITE_GP_IS_DEV"];
-if (!SOCKET) throw Error("socket NVIM missing");
+
+if (!SOCKET) throw Error("missing NVIM socket");
+
+if (!ENV.IS_DEV) {
+    Bun.spawn([normalize(`${import.meta.dir}/../../server/bin`)], {
+        stdio: [null, null, null],
+    });
+}
 
 const nvim = attach({ socket: SOCKET });
+const init = (await nvim.getVar("github_preview_init")) as PluginInit;
 
-const init = await nvim.getVar("github_preview_init");
-console.log("init", init);
-console.log("VITE_GP_IS_DEV", VITE_GP_IS_DEV);
-console.log("SHARED_VAL: ", SHARED_VAL);
+await Bun.connect({
+    unix: GP_UNIX_SOCKET_PATH,
+    socket: {
+        async open(socket) {
+            // as soon as we connect, we send config to server
+            const initEvent: SocketEvent = { type: "init", data: init };
+            socket.write(JSON.stringify(initEvent));
+
+            for (const event of EVENT_NAMES) await nvim.subscribe(event);
+
+            // nvim notifications are forwarded as they are.
+            // It so happens that nvim notification names match
+            // the server's notification names and payload structure
+            nvim.on("notification", (event: string, [arg]: unknown[]) => {
+                socket.write(JSON.stringify({ type: event, data: arg }));
+            });
+        },
+        data(_socket, data) {
+            console.log("data: ", data);
+        },
+        connectError(_socket, _error) {
+            console.log("connection failed, maybe retry?");
+        },
+    },
+});
