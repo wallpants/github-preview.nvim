8a687f0aeb6d399956eb1009a0cf338c259ccfd0
Gual Casas <gual.casas@gmail.com>
2023-09-13 02:03:05 -0600

    typed api

--- typescript/neovim-bridge/src/index.ts
+++ typescript/neovim-bridge/src/index.ts
@@ -1,13 +1,15 @@
-import { attach } from "localbunvim";
+import { NeovimApiInfo, attach } from "localbunvim";
 
 const SOCKET = process.env["NVIM"];
 if (!SOCKET) throw Error("socket missing");
 
-const nvim = await attach({ socket: SOCKET });
-const response = await nvim.call("nvim_get_var", "github_preview_init");
+const nvim = await attach<NeovimApiInfo>({ socket: SOCKET });
+const response = await nvim.call("nvim_get_var", ["github_preview_init"]);
 // const response = await nvim.call("nvim_get_api_info");
 console.log("typeof response: ", typeof response);
 console.log("response: ", response);
+const name = await nvim.call("nvim_buf_get_name", [0]);
+console.log("name: ", name);
 
 // const apiInfo = await nvim.request("nvim_get_api_info");
 // await Bun.write(resolve(import.meta.dir, "out.json"), JSON.stringify(apiInfo));
