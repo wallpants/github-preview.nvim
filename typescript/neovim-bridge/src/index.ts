d4a0cffb8e3d7b4ed8b19dfa6984d0cce1f5c475
Gual Casas <gual.casas@gmail.com>
2023-09-13 11:09:12 -0600

    savepoint

--- typescript/neovim-bridge/src/index.ts
+++ typescript/neovim-bridge/src/index.ts
@@ -1,17 +1,89 @@
+import {
+    createLogger,
+    ENV,
+    GP_UNIX_SOCKET_PATH,
+    type PluginInit,
+    type SocketEvent,
+} from "@gp/shared";
+import { type Socket } from "bun";
 import { attach, type NeovimApiInfo } from "bunvim";
+import { normalize } from "node:path";
+
+const logger = createLogger(ENV.GP_BRIDGE_LOG_STREAM);
+
+const EVENT_NAMES: SocketEvent["type"][] = [
+    "github-preview-init",
+    "github-preview-cursor-move",
+    "github-preview-content-change",
+];
+
+function initializeServer() {
+    if (ENV.IS_DEV) {
+        Bun.spawn(["bun", "dev"], {
+            cwd: normalize(`${import.meta.dir}/../../server`),
+            stdio: [null, null, null],
+        });
+    } else {
+        Bun.spawn(["bun", "start"], {
+            cwd: normalize(`${import.meta.dir}/../../server`),
+            stdio: [null, null, null],
+        });
+    }
+}
 
 const SOCKET = process.env["NVIM"];
 if (!SOCKET) throw Error("socket missing");
 
 const nvim = await attach<NeovimApiInfo>({ socket: SOCKET });
-const response = await nvim.call("nvim_get_var", ["github_preview_init"]);
-// const response = await nvim.call("nvim_get_api_info");
-console.log("typeof response: ", typeof response);
-console.log("response: ", response);
-const name = await nvim.call("nvim_buf_get_name", [0]);
-console.log("name: ", name);
+const init = (await nvim.call("nvim_get_var", ["github_preview_init"])) as PluginInit;
 
-// const apiInfo = await nvim.request("nvim_get_api_info");
-// await Bun.write(resolve(import.meta.dir, "out.json"), JSON.stringify(apiInfo));
+const MAX_ATTEMPTS = 20;
+let attempt = 1;
+let serverInitialized = false;
+let client: Socket | undefined;
 
-// await nvim.request("nvim_subscribe", "github-preview-cursor-move");
+while (attempt <= MAX_ATTEMPTS && (!client || ["closing", "closed"].includes(client.readyState))) {
+    try {
+        /* Attempt to connect to server.
+         * The server may be either uninitialized OR
+         * initialized by a previous lauch of github-preview
+         */
+        logger.verbose(`attempting to connect # ${attempt++}/${MAX_ATTEMPTS}`);
+        client = await Bun.connect({
+            unix: GP_UNIX_SOCKET_PATH,
+            socket: {
+                async open(socket) {
+                    // as soon as we connect, we send config to server
+                    const initEvent: SocketEvent = {
+                        type: "github-preview-init",
+                        data: init,
+                    };
+                    socket.write(JSON.stringify(initEvent));
+
+                    for (const event of EVENT_NAMES) await nvim.call("nvim_subscribe", [event]);
+
+                    /* nvim notifications are forwarded as they are.
+                     * It so happens that nvim notification names match
+                     * the server's notification names and payload structure
+                     */
+                    nvim.onNotification((event, args) => {
+                        console.log("event: ", event);
+                        console.log("args: ", args);
+                    });
+                    // nvim.on("notification", (event: string, [arg]: unknown[]) => {
+                    //     socket.write(JSON.stringify({ type: event, data: arg }));
+                    // });
+                },
+            },
+        });
+        serverInitialized = true;
+    } catch (err) {
+        logger.verbose("failed to connect");
+        if (!serverInitialized) {
+            logger.verbose("starting server");
+            initializeServer();
+            serverInitialized = true;
+        }
+        await Bun.sleep(100);
+    }
+}
