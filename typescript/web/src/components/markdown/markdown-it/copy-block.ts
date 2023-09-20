a450dea41b63102f39e6d96f0dfd697eb4f61a08
Gual Casas <gual.casas@gmail.com>
2023-09-20 13:16:43 -0600

    pass svg directly to fix svg flickering

--- typescript/web/src/components/markdown/markdown-it/copy-block.ts
+++ typescript/web/src/components/markdown/markdown-it/copy-block.ts
@@ -26,9 +26,7 @@ function renderCode(origRule: RenderRule): RenderRule {
         return `
 <div class="copy-button-container">
     <button class="markdown-it-copy-block" data-clipboard-text="${content}">
-        <svg>
-            <use xlink:href="/copy.svg#copy-svg"></use>
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
     </button>
     <span class="copied">copied</span>
 </div>
