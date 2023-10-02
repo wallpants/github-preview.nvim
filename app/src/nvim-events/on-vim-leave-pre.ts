4ec6d5627e2a5faabc303798e4c786adb29523a7
Gual Casas <gual.casas@gmail.com>
2023-10-02 13:44:51 -0600

    remove call to subscribe
    we don't need to subscribe to requests for the client to receive them

--- app/src/nvim-events/on-vim-leave-pre.ts
+++ app/src/nvim-events/on-vim-leave-pre.ts
@@ -5,13 +5,10 @@ export async function onVimLeavePre(
     nvim: Nvim<CustomEvents>,
     callback: (args: CustomEvents["requests"]["VimLeavePre"]) => unknown,
 ) {
-    // Subscribe to RPCNotification
-    await nvim.call("nvim_subscribe", ["CursorMove"]);
-
     // Request handler
     nvim.onRequest("VimLeavePre", callback);
 
-    // Create autocmd to notify us with event "CursorMove"
+    // Create autocmd to make RPCRequest "VimLeavePre"
     const channelId = await nvim.channelId();
     await nvim.call("nvim_create_autocmd", [
         ["VimLeavePre"],
