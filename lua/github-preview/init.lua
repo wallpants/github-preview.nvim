9a2cd6a84b10fa875547cdeed753d7699974820c
wallpants <47203170+wallpants@users.noreply.github.com>
2023-10-24 19:53:38 -0600

    refactor(types): move lua types into own file

--- lua/github-preview/init.lua
+++ lua/github-preview/init.lua
@@ -1,26 +1,5 @@
 local M = {}
 
----@class scroll
----@field disable boolean
----@field top_offset_pct number
-
----@class cursor_line
----@field disable boolean
----@field color string
-
----@class plugin_init
----@field port number
----@field root string
----@field path string
----@field single_file boolean
----@field scroll scroll
----@field cursor_line cursor_line
-
----@class nvim_plugin_opts
----@field port number
----@field scroll scroll
----@field cursor_line cursor_line
-
 ---@type nvim_plugin_opts
 M.default_opts = {
 	port = 6041,
