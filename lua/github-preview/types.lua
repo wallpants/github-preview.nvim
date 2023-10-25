0b232e59f045d7ceee5df3c8dea2ac41184de5db
wallpants <47203170+wallpants@users.noreply.github.com>
2023-10-24 19:53:38 -0600

    refactor(types): move lua types into own file

---
+++ lua/github-preview/types.lua
@@ -0,0 +1,20 @@
+---@class scroll
+---@field disable boolean
+---@field top_offset_pct number
+
+---@class cursor_line
+---@field disable boolean
+---@field color string
+
+---@class plugin_init
+---@field port number
+---@field root string
+---@field path string
+---@field single_file boolean
+---@field scroll scroll
+---@field cursor_line cursor_line
+
+---@class nvim_plugin_opts
+---@field port number
+---@field scroll scroll
+---@field cursor_line cursor_line
