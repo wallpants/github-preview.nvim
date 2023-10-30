ef635b70503f4404bf4cdde00f79e04c3167a24a
wallpants <47203170+wallpants@users.noreply.github.com>
2023-10-30 14:54:15 -0600

    chore(explorer): settings button template

--- app/src/web/explorer/header.tsx
+++ app/src/web/explorer/header.tsx
@@ -2,6 +2,7 @@ import { IconButton } from "../icon-button.tsx";
 import { cn } from "../utils.ts";
 import { PanelCloseIcon } from "./icons/panel-close.tsx";
 import { PanelOpenIcon } from "./icons/panel-open.tsx";
+// import { SettingsIcon } from "./icons/settings.tsx";
 
 export const Header = ({
     isExpanded,
@@ -14,14 +15,22 @@ export const Header = ({
 }) => (
     <div
         className={cn(
-            "flex h-14 items-center border-b border-github-border-default bg-github-canvas-default",
-            isExpanded ? "px-4 justify-between" : "justify-center",
+            "flex items-center border-b border-github-border-default bg-github-canvas-default",
+            isExpanded ? "px-4 justify-between h-14" : "justify-center flex-col-reverse",
             className,
         )}
     >
         {isExpanded && <h4 className="!my-0">Files</h4>}
+        {/* <IconButton */}
+        {/*     className={cn(isExpanded ? "ml-4" : "my-2")} */}
+        {/*     noBorder={!isExpanded} */}
+        {/*     Icon={SettingsIcon} */}
+        {/*     onClick={() => { */}
+        {/*         console.log("open settings"); */}
+        {/*     }} */}
+        {/* /> */}
         <IconButton
-            className={cn(isExpanded && "ml-4")}
+            className={cn(isExpanded ? "ml-4" : "my-2")}
             noBorder={!isExpanded}
             Icon={isExpanded ? PanelOpenIcon : PanelCloseIcon}
             onClick={() => {
