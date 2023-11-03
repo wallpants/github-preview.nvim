510a4eca6194c18179580d9592dbce5f5f7b98e4
wallpants <47203170+wallpants@users.noreply.github.com>
2023-11-03 02:45:25 -0600

    chore(icons): add new icons

---
+++ app/web/components/icons/unfold-vertical.tsx
@@ -0,0 +1,27 @@
+type Props = {
+    className?: string;
+};
+
+export const UnfoldVerticalIcon = ({ className }: Props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={className}
+    >
+        <path d="M12 22v-6" />
+        <path d="M12 8V2" />
+        <path d="M4 12H2" />
+        <path d="M10 12H8" />
+        <path d="M16 12h-2" />
+        <path d="M22 12h-2" />
+        <path d="m15 19-3 3-3-3" />
+        <path d="m15 5-3-3-3 3" />
+    </svg>
+);
