4ca1c4a7523b08f303ebe5c45cd83b80362bb72e
wallpants <47203170+wallpants@users.noreply.github.com>
2023-11-02 19:25:09 -0600

    fix(theme-selector): working theme-selector

--- app/web/__main__.tsx
+++ app/web/__main__.tsx
@@ -5,7 +5,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Explorer } from "./components/explorer/index.tsx";
 import { Markdown } from "./components/markdown/index.tsx";
-import { Provider } from "./components/websocket-provider/provider";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { WebsocketProvider } from "./components/websocket-provider/provider";
 
 // consts defined during Bun build or vite config
 declare const __HOST__: string;
@@ -14,11 +15,13 @@ declare const __DEV__: boolean;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Provider host={__HOST__} port={__PORT__} is_dev={__DEV__}>
-            <div className="flex h-screen w-screen flex-row-reverse overflow-hidden py-3">
-                <Markdown className="mx-4 h-full flex-1 overflow-y-auto overflow-x-hidden" />
-                <Explorer />
-            </div>
-        </Provider>
+        <WebsocketProvider host={__HOST__} port={__PORT__} is_dev={__DEV__}>
+            <ThemeProvider>
+                <div className="flex h-screen w-screen flex-row-reverse overflow-hidden py-3">
+                    <Markdown className="mx-4 h-full flex-1 overflow-y-auto overflow-x-hidden" />
+                    <Explorer />
+                </div>
+            </ThemeProvider>
+        </WebsocketProvider>
     </React.StrictMode>,
 );
--- app/web/components/explorer/config/theme-picker.tsx
+++ app/web/components/explorer/config/theme-picker.tsx
@@ -1,102 +1,36 @@
-import { type Mermaid } from "mermaid";
-import { useEffect, useState, type FC } from "react";
+import { useContext, useState, type FC } from "react";
+import { type Config } from "../../../../types.ts";
+import { useOnDocumentClick } from "../../../use-on-document-click.ts";
 import { IconButton } from "../../icon-button.tsx";
 import { MoonIcon } from "../../icons/moon.tsx";
 import { SunIcon } from "../../icons/sun.tsx";
 import { SystemIcon } from "../../icons/system.tsx";
+import { websocketContext } from "../../websocket-provider/context.ts";
 
-type Theme = "dark" | "light";
-type Selected = Theme | "system";
-
-const iconsMap: Record<Selected, FC<{ className: string }>> = {
+const iconsMap: Record<Config["theme"], FC<{ className: string }>> = {
     system: SystemIcon,
     light: SunIcon,
     dark: MoonIcon,
 };
 
-declare const mermaid: Mermaid;
-
-function getSystemTheme(): Theme {
-    return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-}
-
 export function ThemePicker() {
-    const [selected, setSelected] = useState<Selected>("system");
-    const [system, setSystem] = useState<Theme>(getSystemTheme());
-    const [override, setOverride] = useState<Theme | null>(null);
+    const { config, wsRequest } = useContext(websocketContext);
     const [isOpen, setIsOpen] = useState(false);
 
-    const theme = override ?? system;
+    const theme = config?.overrides.theme ?? "system";
 
-    useEffect(() => {
-        const local = localStorage.getItem("override") as Theme | null;
-        setOverride(local);
-        if (local) setSelected(local);
-
-        setSystem(getSystemTheme());
-    }, []);
-
-    useEffect(() => {
-        function handleThemeChange(theme: Theme) {
-            const rootHtml = document.getElementsByTagName("html")[0]!;
-            rootHtml.className = `pantsdown ${theme}`;
-            mermaid.initialize({
-                startOnLoad: false,
-                theme: theme === "light" ? "default" : "dark",
-            });
-        }
-
-        handleThemeChange(theme);
-    }, [theme]);
-
-    useEffect(() => {
-        if (selected === "system") {
-            localStorage.removeItem("override");
-            setOverride(null);
-        } else {
-            localStorage.setItem("override", selected);
-            setOverride(selected);
-        }
-    }, [selected]);
-
-    useEffect(() => {
-        function eventHandler({ matches }: { matches: boolean }) {
-            const newTheme = matches ? "dark" : "light";
-            setSystem(newTheme);
-        }
-
-        window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", eventHandler);
-
-        return () => {
-            window
-                .matchMedia("(prefers-color-scheme: dark)")
-                .removeEventListener("change", eventHandler);
-        };
-    }, [override]);
-
-    useEffect(() => {
-        if (!isOpen) return;
-        // setup listener to close menu on click anywhere
-        const controller = new AbortController();
-
-        document.addEventListener(
-            "click",
-            () => {
-                setIsOpen(false);
-            },
-            { signal: controller.signal },
-        );
-
-        return () => {
-            controller.abort();
-        };
-    }, [isOpen]);
+    useOnDocumentClick({
+        disabled: !isOpen,
+        callback: () => {
+            setIsOpen(false);
+        },
+    });
 
     return (
         <div className="relative z-20">
             <IconButton
                 noBorder={!isOpen}
-                Icon={iconsMap[selected]}
+                Icon={iconsMap[theme]}
                 onClick={(e) => {
                     e.stopPropagation();
                     setIsOpen(!isOpen);
@@ -105,16 +39,21 @@ export function ThemePicker() {
             {isOpen && (
                 <div className="absolute left-[102%] top-0 flex space-x-[1%]">
                     {Object.keys(iconsMap)
-                        .filter((theme) => theme !== selected)
+                        .filter((t) => t !== theme)
                         .map((theme) => (
                             <IconButton
                                 key={theme}
                                 className="bg-github-canvas-default"
-                                Icon={iconsMap[theme as Selected]}
+                                Icon={iconsMap[theme as Config["theme"]]}
                                 onClick={(e) => {
                                     e.stopPropagation();
-                                    setSelected(theme as Selected);
                                     setIsOpen(!isOpen);
+                                    wsRequest({
+                                        type: "update-config",
+                                        config: {
+                                            theme: theme as Config["theme"],
+                                        },
+                                    });
                                 }}
                             />
                         ))}
