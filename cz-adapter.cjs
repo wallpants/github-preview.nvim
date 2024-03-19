24d9ddea1f84cb453e64d97f8f78a1e2d2e0e2a8
wallpants <47203170+wallpants@users.noreply.github.com>
2024-03-18 18:54:02 -0600

    perf(deps): udpate dependencies & lockfile

--- cz-adapter.cjs
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-// this bridge adapter as workaround for ESM version of commitlint
-// https://github.com/conventional-changelog/commitlint/issues/3949
-
-// @ts-ignore
-exports.prompter = async (inquirerIns, commit) => {
-    (await import("@commitlint/cz-commitlint")).prompter(inquirerIns, commit);
-};
