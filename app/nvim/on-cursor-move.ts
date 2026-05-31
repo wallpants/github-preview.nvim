27bb00b830668f784e7bd074bfdc33ff3ce54d8e
wallpants <47203170+wallpants@users.noreply.github.com>
2026-05-31 03:03:00 -0600

    style(formatter): run formatter

--- app/nvim/on-cursor-move.ts
+++ app/nvim/on-cursor-move.ts
@@ -11,20 +11,20 @@ export async function onCursorMove(
    // Notification handler
    app.nvim.onNotification(NOTIFICATION, callback);
 
-    // Create autocmd to notify us with event "CursorMove"
-    await app.nvim.call("nvim_create_autocmd", [
-        ["CursorHold", "CursorHoldI"],
-        {
-            group: app.augroupId,
-            desc: "Notify github-preview",
-            command: `lua
-                local buftype = vim.api.nvim_get_option_value("buftype", { buf = 0 })
-                if buftype == "" then
-                    local buffer = vim.api.nvim_get_current_buf()
-                    local path = vim.api.nvim_buf_get_name(0)
-                    local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
-                    vim.rpcnotify(${app.nvim.channelId}, "${NOTIFICATION}", buffer, path, cursor_line)
-                end`,
-        },
-    ]);
+   // Create autocmd to notify us with event "CursorMove"
+   await app.nvim.call("nvim_create_autocmd", [
+      ["CursorHold", "CursorHoldI"],
+      {
+         group: app.augroupId,
+         desc: "Notify github-preview",
+         command: `lua
+            local buftype = vim.api.nvim_get_option_value("buftype", { buf = 0 })
+            if buftype == "" then
+               local buffer = vim.api.nvim_get_current_buf()
+               local path = vim.api.nvim_buf_get_name(0)
+               local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
+               vim.rpcnotify(${app.nvim.channelId}, "${NOTIFICATION}", buffer, path, cursor_line)
+            end`,
+      },
+   ]);
 }
